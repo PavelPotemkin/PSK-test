aef772216cb86628a5be115f17640031629f83ce
Pavel <p.potemkin.work@gmail.com>
2022-05-06 00:15:49 +0500

    feat: add svg sprite and close icon

---
+++ src/ui/UiSvgIcon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  iconName: string;
+}>();
+
+let icon = require(`@/assets/icons/${props.iconName}.svg`);
+if (Object.prototype.hasOwnProperty.call(icon, "default")) {
+  icon = icon.default;
+}
+
+const iconPath = icon.url;
+</script>
+
+<template>
+  <svg class="ui-svg-icon" xmlns="http://www.w3.org/2000/svg">
+    <use :href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink" />
+  </svg>
+</template>
+
+<style scoped>
+.ui-svg-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
